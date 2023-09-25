b1314bb5e127ebbdda57d7339140badd0671bd8c
Jose Luis Getafe <email@email.es>
2023-09-25 17:43:03 +0200

    Solucion ejercicio Escaparate

--- src/es/curso/java/poo/ejercicios/maniqui/EscaparateProfe.java
+++ src/es/curso/java/poo/ejercicios/maniqui/EscaparateProfe.java
@@ -23,12 +23,71 @@ public class EscaparateProfe {
 		
 		EscaparateProfe escaparate = new EscaparateProfe();
 		escaparate.mostrarEscaparate(maniqui1, maniqui2);
+		
+		ManiquiProfe [] maniquis = {maniqui1, maniqui2};
+		escaparate.mostrarPrecionManiquis(maniquis);
 	}
 	
 	public void mostrarEscaparate (ManiquiProfe...maniquis) {
 		for (ManiquiProfe maniquiProfe : maniquis) {
 			
+			System.out.println(maniquiProfe.getId());
+			if (maniquiProfe.getCamisa() != null) {
+				System.out.println("Datos de la camisa");
+				System.out.println(maniquiProfe.getCamisa().getColor());
+				System.out.println(maniquiProfe.getCamisa().getTalla());
+				System.out.println(maniquiProfe.getCamisa().getPrecio());
+				
+				Boton [] arrayBotones = maniquiProfe.getCamisa().getBotones();
+				for (Boton boton : arrayBotones) {
+					System.out.println(boton.getColor());
+					System.out.println(boton.getForma());
+				}
+				
+			}
+			
+			
+			if (maniquiProfe.getPantalon() != null) {
+				System.out.println("Datos del pantalon");
+				System.out.println(maniquiProfe.getPantalon().getColor());
+				System.out.println(maniquiProfe.getPantalon().getTalla());
+				System.out.println(maniquiProfe.getPantalon().getPrecio());
+				
+				Boton botonPantalon = maniquiProfe.getPantalon().getBoton();
+				System.out.println(botonPantalon.getColor());
+				System.out.println(botonPantalon.getForma());
+			}
+			
+			if (maniquiProfe.getVestido() != null) {
+				System.out.println("Datos del vestido");
+				System.out.println(maniquiProfe.getVestido().getColor());
+				System.out.println(maniquiProfe.getVestido().getTalla());
+				System.out.println(maniquiProfe.getVestido().getPrecio());
+
+			}
+			
+			
 		}
 	}
 
+	public void mostrarPrecionManiquis (ManiquiProfe [] maniquis) {
+		double precioTotal = 0;
+		for (ManiquiProfe  maniquiProfe: maniquis) {
+			precioTotal = 0;
+			if (maniquiProfe.getVestido() != null) {
+				double precioVestido = maniquiProfe.getVestido().getPrecio();
+				precioTotal += precioVestido;
+			}
+			if (maniquiProfe.getPantalon() != null) {
+				precioTotal += maniquiProfe.getPantalon().getPrecio();
+			}
+			if (maniquiProfe.getCamisa() != null) {
+				precioTotal += maniquiProfe.getCamisa().getPrecio();
+			}
+			System.out.println("El precio total es de "+precioTotal);
+		}
+		
+		
+	}
+		
 }
