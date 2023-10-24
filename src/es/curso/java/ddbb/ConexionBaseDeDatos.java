da0f16b0ff9423e285eb249f577657b98b5afc90
Jose Luis Getafe <email@email.es>
2023-10-24 17:21:17 +0200

    Metodo con try with resources

--- src/es/curso/java/ddbb/ConexionBaseDeDatos.java
+++ src/es/curso/java/ddbb/ConexionBaseDeDatos.java
@@ -7,29 +7,61 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class ConexionBaseDeDatos {
-	
-	
+
 	public static void main(String[] args) {
 		ConexionBaseDeDatos conexion = new ConexionBaseDeDatos();
-		conexion.conecta();
-		
+		conexion.conectaTryWithResources();
+
 	}
 	
+	private void conectaTryWithResources() {
+		String url = "jdbc:mysql://localhost:3306/curso?serverTimezone=Europe/Madrid";
+		String username = "root";
+		String password = "password";
+
+		//Connection connection = null;//Conexion con la base de datos
+		//Statement stmt = null;//Lanzar consulta
+		//ResultSet rs = null;//Recoger datos de la consulta
+		
+		try (
+				Connection connection = DriverManager.getConnection(url, username, password);
+				Statement stmt = connection.createStatement();
+				ResultSet rs = stmt.executeQuery("SELECT * FROM TB_FACILITIES");
+			) {
+
+			
+			while (rs.next()) {
+				System.out.println(rs.getLong("id"));
+				System.out.println(rs.getInt("guid"));
+				System.out.println(rs.getString("name"));
+				System.out.println(rs.getString("description"));
+				System.out.println(rs.getString("category"));
+				System.out.println("===============================");
+			}
+
+		} catch (SQLException e) {
+			System.err.println("Ha habido un error " + e.getMessage());
+//			e.printStackTrace();
+		} 
+
+	}
+	
+
 	private void conecta() {
 		String url = "jdbc:mysql://localhost:3306/curso?serverTimezone=Europe/Madrid";
-	    String username = "root";
-	    String password = "password";
-	    
-	    Connection connection = null;
-	    Statement stmt = null;
-	    ResultSet rs = null;
-	    
+		String username = "root";
+		String password = "password";
+
+		Connection connection = null;//Conexion con la base de datos
+		Statement stmt = null;//Lanzar consulta
+		ResultSet rs = null;//Recoger datos de la consulta
 		try {
+
 			System.out.println("Estableciendo conexión");
 			connection = DriverManager.getConnection(url, username, password);
 			System.out.println("Conexión establecida");
 			stmt = connection.createStatement();
-			
+
 			rs = stmt.executeQuery("SELECT * FROM TB_FACILITIES");
 			while (rs.next()) {
 				System.out.println(rs.getLong("id"));
@@ -38,21 +70,26 @@ public class ConexionBaseDeDatos {
 				System.out.println(rs.getString("description"));
 				System.out.println(rs.getString("category"));
 				System.out.println("===============================");
-            }		
-			
+			}
+
 		} catch (SQLException e) {
-			System.err.println("Ha habido un error "+e.getMessage());
+			System.err.println("Ha habido un error " + e.getMessage());
 //			e.printStackTrace();
-		}finally {
+		} finally {
+			
 			try {
-				connection.close();
-				stmt.close();
-				rs.close();
+				if (connection!=null) {
+					connection.close();
+				}
+				if (stmt!=null)
+					stmt.close();
+				if (rs!=null)
+					rs.close();
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
-		
+
 	}
 }
