21b6155524015506b810f1a5422a924052362a7c
Jose Luis Getafe <email@email.es>
2023-11-02 20:58:49 +0100

    PrepareStaments Inserts

--- src/es/curso/java/ddbb/ConexionBaseDeDatos.java
+++ src/es/curso/java/ddbb/ConexionBaseDeDatos.java
@@ -2,6 +2,7 @@ package es.curso.java.ddbb;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -14,7 +15,7 @@ public class ConexionBaseDeDatos {
 
 	public static void main(String[] args) {
 		ConexionBaseDeDatos conexion = new ConexionBaseDeDatos();
-		conexion.conectaTryWithResources();
+		conexion.insertPrepareStatement();
 
 	}
 	
@@ -70,6 +71,7 @@ public class ConexionBaseDeDatos {
 			stmt = connection.createStatement();
 
 			rs = stmt.executeQuery("SELECT * FROM TB_FACILITIES");
+			
 			while (rs.next()) {
 				System.out.println(rs.getLong("id"));
 				System.out.println(rs.getInt("guid"));
@@ -99,4 +101,114 @@ public class ConexionBaseDeDatos {
 		}
 
 	}
+	
+	
+	private void conectaPrepareStatement() {
+		String url = "jdbc:mysql://localhost:3306/curso?serverTimezone=Europe/Madrid";
+		String username = "root";
+		String password = "password";
+
+		Connection connection = null;//Conexion con la base de datos
+		//Statement stmt = null;//Lanzar consulta
+		PreparedStatement prepareStament=null;
+		ResultSet rs = null;//Recoger datos de la consulta
+		try {
+
+			System.out.println("Estableciendo conexión");
+			connection = DriverManager.getConnection(url, username, password);
+			System.out.println("Conexión establecida");
+			
+			String address = "DIR2";
+			prepareStament = connection.prepareStatement(
+					"SELECT * FROM TB_FACILITIES WHERE ADDRESS=? AND ID=?");
+			prepareStament.setString(1, address);
+			prepareStament.setInt(2, 100);
+			
+			
+			rs = prepareStament.executeQuery();
+			
+			while (rs.next()) {
+				System.out.println(rs.getLong("id"));
+				System.out.println(rs.getInt("guid"));
+				System.out.println(rs.getString("name"));
+				System.out.println(rs.getString("description"));
+				System.out.println(rs.getString("category"));
+				System.out.println("===============================");
+			}
+
+		} catch (SQLException e) {
+			System.err.println("Ha habido un error " + e.getMessage());
+//			e.printStackTrace();
+		} finally {
+			
+			try {
+				if (connection!=null) {
+					connection.close();
+				}
+				if (prepareStament!=null)
+					prepareStament.close();
+				if (rs!=null)
+					rs.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
+	
+	private void insertPrepareStatement() {
+		String url = "jdbc:mysql://localhost:3306/curso?serverTimezone=Europe/Madrid";
+		String username = "root";
+		String password = "password";
+
+		Connection connection = null;//Conexion con la base de datos
+		//Statement stmt = null;//Lanzar consulta
+		PreparedStatement prepareStament=null;
+		ResultSet rs = null;//Recoger datos de la consulta
+		try {
+
+			System.out.println("Estableciendo conexión");
+			connection = DriverManager.getConnection(url, username, password);
+			System.out.println("Conexión establecida");
+			
+			String insert = "INSERT INTO TB_FACILITIES "
+					+ "(guid,name,description,category,address,heigth)\r\n"
+					+ "VALUES (?,?,?,?,?,?)";
+			
+			prepareStament = connection.prepareStatement(insert);
+			prepareStament.setInt(1, 2314);
+			prepareStament.setString(2, "EDIFICIO3");
+			prepareStament.setString(3, "DESC EDIFICIO3");
+			prepareStament.setString(4, "CATEGORIA");
+			prepareStament.setString(5, "ADDRESS");
+			prepareStament.setInt(6, 90);
+			
+			
+			boolean insertado = prepareStament.execute();
+			System.out.println("Insertado: "+insertado);
+			
+
+		} catch (SQLException e) {
+			System.err.println("Ha habido un error " + e.getMessage());
+//			e.printStackTrace();
+		} finally {
+			
+			try {
+				if (connection!=null) {
+					connection.close();
+				}
+				if (prepareStament!=null)
+					prepareStament.close();
+				if (rs!=null)
+					rs.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
+	
+	
 }
