da7cf49eb75cf3e0e38860feea4e07f3acc5f640
Jose Luis Getafe <email@email.es>
2023-11-16 20:14:07 +0100

    Ejemplos archivos modificado

--- src/es/curso/java/ficheros/ArchivoServicio.java
+++ src/es/curso/java/ficheros/ArchivoServicio.java
@@ -7,51 +7,55 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Scanner;
 
 public class ArchivoServicio {
 
-    public void crearArchivo(String nombre){
-        File archivo = new File(nombre);
-        try (BufferedWriter buffer = new BufferedWriter(new FileWriter(archivo, false))){
+	public void crearArchivo(String nombre) {
+		File archivo = new File(nombre);
+		try (BufferedWriter buffer = new BufferedWriter(new FileWriter(archivo, false))) {
 
-            buffer.append("Hola que tal amigos!\n")
-                    .append("Todo bien? yo escribiendo en un archivo...\n")
-                    .append("Hasta luego Lucas!\n");
-            // buffer.close();
-            System.out.println("El archivo se ha creado con éxito!");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+			buffer.append("Hola que tal amigos!\n").append("Todo bien? yo escribiendo en un archivo...\n")
+					.append("Hasta luego Lucas!\n");
+			// buffer.close();
+			System.out.println("El archivo se ha creado con éxito!");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-    public void crearArchivo2(String nombre){
-        File archivo = new File(nombre);
-        
-        try (PrintWriter buffer = new PrintWriter(new FileWriter(archivo))){
+	public void crearArchivo2(String nombre) {
+		File archivo = new File(nombre);
 
-            buffer.println("Hola que tal amigos!");
-            buffer.println("Todo bien? yo acá escribiendo un archivo...");
-            buffer.printf("Hasta luego %s!", "JoseLuis5");
-            // buffer.close();
-            System.out.println("El archivo se ha creado con éxito!");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+		try (PrintWriter buffer = new PrintWriter(archivo)) {
+			// try (PrintWriter buffer = new PrintWriter(new FileWriter(archivo))){
 
-    public String leerArchivo(String nombre) throws IOException{
-        StringBuilder sb = new StringBuilder();
-        File archivo = new File(nombre);
-        FileReader fileReader = new FileReader(archivo);
-        BufferedReader reader = new BufferedReader(fileReader);
-        
-        String linea;
-        while ( (linea = reader.readLine()) != null){
-            sb.append(linea).append("\n");
-        }
-        
+			buffer.println("Hola que tal amigos!");
+			buffer.println("Todo bien? yo acá escribiendo un archivo...");
+			buffer.printf("Hasta luego %s!", "JoseLuis5");
+			// buffer.close();
+			System.out.println("El archivo se ha creado con éxito!");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public String leerArchivo(String nombre) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		File archivo = new File(nombre);
+		FileReader fileReader = new FileReader(archivo);
+		BufferedReader reader = new BufferedReader(fileReader);
+
+		String linea;
+		while ((linea = reader.readLine()) != null) {
+			sb.append(linea).append("\n");
+		}
+
 //        try (BufferedReader reader = new BufferedReader(new FileReader(archivo))){
 //
 //            String linea;
@@ -61,52 +65,62 @@ public class ArchivoServicio {
 //        } catch (IOException e) {
 //            e.printStackTrace();
 //        }
-        return sb.toString();
-    }
+		return sb.toString();
+	}
 
-    public String leerArchivo2(String nombre){ 
-        StringBuilder sb = new StringBuilder();
-        File archivo = new File(nombre);
-        try (Scanner s = new Scanner(archivo)){
+	public String leerArchivo2(String nombre) {
+		StringBuilder sb = new StringBuilder();
+		File archivo = new File(nombre);
+		try (Scanner s = new Scanner(archivo)) {
 
-            s.useDelimiter("\n");
-            while (s.hasNext()){
-                sb.append(s.next()).append("\n");
-            }
+			s.useDelimiter("\n");
+			while (s.hasNext()) {
+				sb.append(s.next()).append("\n");
+			}
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return sb.toString();
-    }
-    
-    
-    public void mostrarContenidoDirectorio(String directorio) {
-    	File dir = new File(directorio);
-    	File file1 = new File(directorio,"fichero1.txt");
-    	File file2 = new File(dir,"fichero1.txt");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return sb.toString();
+	}
 
+	public String leerArchivo3(String nombre) throws IOException{
+		Path path = Paths.get(nombre);
+		StringBuilder sb = new StringBuilder();
 
-        if(dir.isDirectory()) {
-        	String[] ficheros = dir.list();
-        	for (String nombreFichero : ficheros) {
-				//System.out.print(nombreFichero + " ");
-				String nuevoFichero = directorio+nombreFichero;
+		List<String> lines = Files.readAllLines(path);
+		for (String linea : lines) {
+			sb.append(linea+"\n");
+		}
+
+		return sb.toString();
+	}
+
+	public void mostrarContenidoDirectorio(String directorio) {
+		File dir = new File(directorio);
+		File file1 = new File(directorio, "fichero1.txt");
+		File file2 = new File(dir, "fichero1.txt");
+
+		if (dir.isDirectory()) {
+			String[] ficheros = dir.list();
+			for (String nombreFichero : ficheros) {
+				// System.out.print(nombreFichero + " ");
+				String nuevoFichero = directorio + nombreFichero;
 				System.out.println(nuevoFichero);
 				File file = new File(nuevoFichero);
-				if(file.isDirectory()) {
-					//System.out.println("Es un directorio");
-				}else {
+				if (file.isDirectory()) {
+					// System.out.println("Es un directorio");
+				} else {
 					if (file.getName().endsWith(".txt")) {
-						System.out.println("Se puede leer:"+file.canRead());
-						System.out.println("TAmaño " +file.length());
+						System.out.println("Se puede leer:" + file.canRead());
+						System.out.println("TAmaño " + file.length());
 						System.out.println("Es un fichero");
 					}
 				}
-					
+
 			}
-        }else {
-        	System.err.println("No es un directorio");
-        }
-    }
+		} else {
+			System.err.println("No es un directorio");
+		}
+	}
 }
--- src/es/curso/java/ficheros/EjemploCrearArchivo.java
+++ src/es/curso/java/ficheros/EjemploCrearArchivo.java
@@ -1,12 +1,22 @@
 package es.curso.java.ficheros;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+
 public class EjemploCrearArchivo {
     public static void main(String[] args) {
+    	LocalDateTime datetime = LocalDateTime.now();
+    	System.out.println(datetime.getYear());
+    	System.out.println(datetime.getMonthValue());
+    	System.out.println(datetime.getMonth());
+    	
+    	Instant instant = Instant.now();
+    	System.out.println(instant);
     	//Ruta relativa a la carpeta del proyecto
         String nombreArchivo = "./recursos/java2.txt";
 
         ArchivoServicio service = new ArchivoServicio();
-        service.mostrarContenidoDirectorio("C:\\Users\\Tardes\\");
+        //service.mostrarContenidoDirectorio("C:\\Users\\Tardes\\");
         //service.crearArchivo2(nombreArchivo);
     }
 }
