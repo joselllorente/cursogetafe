7cfa7415bf98fe3dd39d40622d3c5ee8cddde462
Jose Luis Getafe <email@email.es>
2023-11-28 18:47:52 +0100

    Conexion BBDD Oracle con Maven

--- src/es/curso/java/ddbb/ConexionBaseDeDatos.java
+++ src/es/curso/java/ddbb/ConexionBaseDeDatos.java
@@ -10,12 +10,13 @@ import java.util.LinkedHashSet;
 import java.util.Set;
 
 import es.curso.java.ddbb.ejercicios.Facility;
+import oracle.jdbc.pool.OracleDataSource;
 
 public class ConexionBaseDeDatos {
 
 	public static void main(String[] args) {
 		ConexionBaseDeDatos conexion = new ConexionBaseDeDatos();
-		conexion.conectaOracle();
+		conexion.conectaOracle2();
 
 	}
 	
@@ -213,7 +214,6 @@ public class ConexionBaseDeDatos {
 	
 	private void conectaOracle() {
 		String url_oracle = "jdbc:oracle:thin:curso/password@localhost:1521:XE";
-		
 		String url_oracle2 = "jdbc:oracle:thin:@localhost:1521:XE";
 		String username = "curso";
 		String password = "password";
@@ -221,7 +221,7 @@ public class ConexionBaseDeDatos {
 		
 		try  {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-			Connection connection = DriverManager.getConnection(url_oracle2,username,password);
+			Connection connection = DriverManager.getConnection(url_oracle);
 			if (connection!=null) {
 				System.out.println("Conexion establecida");
 			}
@@ -230,6 +230,29 @@ public class ConexionBaseDeDatos {
 			System.err.println("Ha habido un error " + e.getMessage());
 //			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+	
+	private void conectaOracle2() {
+		String url_oracle = "jdbc:oracle:thin:curso/password@localhost:1521:XE";
+		String url_oracle2 = "jdbc:oracle:thin:@localhost:1521:XE";
+		String username = "curso";
+		String password = "password";
+
+		try {
+			OracleDataSource ods = new OracleDataSource();
+			ods.setURL(url_oracle);
+			
+			Connection connection = ods.getConnection();
+			if (connection!=null) {
+				System.out.println("Conexion establecida");
+			}
+			
+		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
