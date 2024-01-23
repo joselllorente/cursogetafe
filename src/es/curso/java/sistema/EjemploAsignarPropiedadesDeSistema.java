ac8029dd0487e123cf0a7720d00506ade340e4ff
jlllorente <correojl@gmail.com>
2024-01-23 16:21:47 +0100

    Ejemplos Clases SIstema Java

---
+++ src/es/curso/java/sistema/EjemploAsignarPropiedadesDeSistema.java
@@ -0,0 +1,30 @@
+package es.curso.java.sistema;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Properties;
+
+public class EjemploAsignarPropiedadesDeSistema {
+    public static void main(String[] args) {
+
+        try {
+            FileInputStream archivo = new FileInputStream("src/config.properties");
+
+            Properties p = new Properties(System.getProperties());
+            p.load(archivo);
+            p.setProperty("mi.propiedad.personalizada","Mi valor guardado en el objeto properties");
+            System.setProperties(p);
+
+            Properties properties = System.getProperties();
+            System.out.println("ps.getProperty(...) = " + properties.getProperty("mi.propiedad.personalizada"));
+            System.out.println(System.getProperty("config.puerto.servidor"));
+            System.out.println(System.getProperty("config.autor.nombre"));
+            System.out.println(System.getProperty("config.autor.email"));
+
+            properties.list(System.out);
+        } catch(Exception e){
+            System.err.println("no existe el archivo = " + e);
+            System.exit(1);
+        }
+    }
+}
