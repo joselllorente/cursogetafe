8e05b3291d27975dea133294db28c1e22d3fca22
jlllorente <correojl@gmail.com>
2024-01-23 17:33:12 +0100

    Modificado

--- src/es/curso/java/sistema/EjemploAsignarPropiedadesDeSistema.java
+++ src/es/curso/java/sistema/EjemploAsignarPropiedadesDeSistema.java
@@ -1,27 +1,31 @@
 package es.curso.java.sistema;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class EjemploAsignarPropiedadesDeSistema {
     public static void main(String[] args) {
 
         try {
-            FileInputStream archivo = new FileInputStream("src/config.properties");
+            InputStream archivo = new FileInputStream("src/config.properties");
 
             Properties p = new Properties(System.getProperties());
             p.load(archivo);
             p.setProperty("mi.propiedad.personalizada","Mi valor guardado en el objeto properties");
+            System.out.println(p.getProperty("config.autor.nombre"));
+            System.out.println(p.getProperty("mi.propiedad.personalizada"));
+            
+            
             System.setProperties(p);
-
-            Properties properties = System.getProperties();
-            System.out.println("ps.getProperty(...) = " + properties.getProperty("mi.propiedad.personalizada"));
-            System.out.println(System.getProperty("config.puerto.servidor"));
-            System.out.println(System.getProperty("config.autor.nombre"));
-            System.out.println(System.getProperty("config.autor.email"));
-
-            properties.list(System.out);
+//
+//            Properties properties = System.getProperties();
+//            System.out.println("ps.getProperty(...) = " + properties.getProperty("mi.propiedad.personalizada"));
+//            System.out.println(System.getProperty("config.puerto.servidor"));
+//            System.out.println(System.getProperty("config.autor.nombre"));
+//            System.out.println(System.getProperty("config.autor.email"));
+//
+//            properties.list(System.out);
         } catch(Exception e){
             System.err.println("no existe el archivo = " + e);
             System.exit(1);
